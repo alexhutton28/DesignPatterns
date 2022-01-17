fd23c9cab48118aa55a7da3a3835fd3f358de072
Alex Hutton <amhutton@email.sc.edu>
2022-01-17 14:34:05 -0500

    Made It Look Nicer

--- Strategy/Driver.java
+++ Strategy/Driver.java
@@ -1,6 +1,6 @@
-package DesignPatterns;
+package DesignPatterns.Strategy;
 
-public class Hello2 {
+public class Driver {
     public static void main(String[] args) {
         System.out.println("Hellooooooo!");
         System.out.println("Hello");
