a30261c71197d245871e03da050c5fc4af0d4488
Alex Hutton <amhutton@email.sc.edu>
2022-01-17 17:03:18 -0500

    Strategy Design Pattern

---
+++ Strategy/SortBehavior.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public interface SortBehavior {
+    /**
+     * Takes in an unsorted array list and returns a sorted array List
+     * 
+     * @param data The Array List that will be sorted
+     * @return The sorted Array List
+     */
+    public ArrayList<String> sort(ArrayList<String> data);
+}
