eead74685fe69298d32ecccb479c112e7ff03e31
Alex Hutton <amhutton@email.sc.edu>
2022-01-17 14:21:24 -0500

    It Works now kindof

--- Hello2.java
+++ Hello2.java
@@ -3,5 +3,6 @@ package DesignPatterns;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Hellooooooo!");
+        System.out.println("Hello");
     }
 }
